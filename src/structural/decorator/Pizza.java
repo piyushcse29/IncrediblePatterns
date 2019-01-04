b42f7110eaf655d7ef506659cdfb708b8839ed5d
Piyush Mittal <m@piyushmittal.com>
2019-01-04 21:46:22 +0100

    changes

---
+++ src/structural/decorator/Pizza.java
@@ -0,0 +1,11 @@
+package structural.decorator;
+
+// Blueprint for classes that will have decorators
+
+public interface Pizza {
+
+    public String getDescription();
+
+    public double getCost();
+
+}
